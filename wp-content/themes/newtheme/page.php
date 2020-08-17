55dd9da7435212c008f9e37926e6b361408c3afd
GeneHFang <gene.li@nyu.edu>
2020-08-16 20:24:06 -0400

    added example scripts and css

--- wp-content/themes/newtheme/page.php
+++ wp-content/themes/newtheme/page.php
@@ -1,18 +1,9 @@
 
-<h1><?php bloginfo('name');?></h1>
-<?php bloginfo('description');?>
-
-<a href="../">home</a>
-page, not post
 <?php
 
-    while(have_posts()){
-        the_post(); ?>
-        <h1><?php the_title()?></h1>
-        <?php the_content()?>
+    get_header();
 
-     <?php
-    }
+    
 
-
+    get_footer();
 ?>
--- wp-content/themes/newtheme/single.php
+++ wp-content/themes/newtheme/single.php
@@ -1,20 +1,12 @@
 <?php 
     //only works for posts, not pages
 ?>
-<h1><?php bloginfo('name');?></h1>
-<?php bloginfo('description');?>
 
-<a href="../">home</a>
 
 <?php
 
-    while(have_posts()){
-        the_post(); ?>
-        <h1><?php the_title()?></h1>
-        <?php the_content()?>
+    get_header();
 
-     <?php
-    }
-
-
+  
+    get_footer();
 ?>
